9308481972da261918aa7837ebf2f83e784fa7ab
Danni <d.youziel@gmail.com>
2018-02-06 07:17:34 +0000

    update_060218

--- js/index.js
+++ js/index.js
@@ -107,6 +107,7 @@ $('.learnlater').on('click', function() {
   $('.carousel').show();
   $('.buttons').show();
   $('.moretxt.' + artist).hide();
+  $('video')[0].pause();
 })
 
 
@@ -117,7 +118,9 @@ BEATS
 $(function() {
   $(this).keydown(function(event) {
     var key = $(this).find('.key[data-key='+event.which+']');
+    console.log(key);
     var audio = $(this).find('audio[data-key='+event.which+']')[0];
+    console.log(audio);
     key.toggleClass('playing');
     if (!audio) return;
     audio.play();
@@ -131,13 +134,82 @@ $(this).keyup(function(event) {
 
 
 $(function() {
-  $(this).click(function(event) {
-    var key = $(this).find('.key[data-key='+event.which+']');
-    var audio = $(this).find('audio[data-key='+event.which+']')[0];
-    key.toggleClass('playing');
-    if (!audio) return;
-    audio.play();
-    audio.currentTime = 0;
+  $('.key').click(function(event) {
+    var key = $(this).data('key');
+    var audio = $('.a' + key)[0];
+     console.log(audio);
+     $('.key' + key).toggleClass('playing');
+     if (!audio) return;
+     audio.play();
+     audio.currentTime = 0;
   });
 });
 
+
+// $(function() {
+//   $(".65").click(function(event) {
+//      var audio = $(".a65")[0];
+//      audio.play();
+//      audio.currentTime = 0;
+// })
+// });
+
+// $(function() {
+//   $(".83").click(function(event) {
+//      var audio = $(".a83")[0];
+//      audio.play();
+// });
+// });
+
+// $(function() {
+//   $(".68").on("click", function() {
+//      var x = $(".a68")[0];
+//      x.play();
+// });
+// });
+
+// $(function() {
+//   $(".70").on("click", function() {
+//      var audio = $(".a70")[0];
+//      audio.play();
+// });
+// });
+
+
+// $(function() {
+//   $(".71").on("click", function() {
+//      var audio = $(".a71")[0];
+//      audio.play();
+// });
+// });
+
+
+// $(function() {
+//   $(".72").on("click", function() {
+//      var audio = $(".a72")[0];
+//      audio.play();
+// });
+// });
+
+// $(function() {
+//   $(".74").on("click", function() {
+//      var audio = $(".a74")[0];
+//      audio.play();
+// });
+// });
+
+
+// $(function() {
+//   $(".75").on("click", function() {
+//      var audio = $(".a75")[0];
+//      audio.play();
+// });
+// });
+
+
+// $(function() {
+//   $(".76").on("click", function() {
+//      var audio = $(".a76")[0];
+//      audio.play();
+// });
+// });
